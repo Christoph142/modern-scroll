63c0434973be66fb9e86bfb81828b5a11d11ad51
Christoph <Christoph>
2012-10-07 23:08:26 +0200

    language updates

---
+++ locales/fr/strings.js
@@ -0,0 +1,18 @@
+var strings = {
+	"by" : "de",
+	"Options" : "Options",
+	"look" : "Apparence",
+	"color" : "Couleur",
+	"size" : "Largeur (respectivement la hauteur des barres horizontales) à l'état normal",
+	"hover_size" : "Largeur (respectivement hauteur de barres horizontales) lorsque planait avec la souris",
+	"border_radius" : "rayon de courbure de la bordure",
+	"no_bar_bg" : "masquer les barres d'arrière-plan",
+	"behavior" : "Comportement",
+	"show_when" : "Quand veux-tu les barres de se présenter",
+	"only_over" : "seulement lorsque planait avec la souris",
+	"normal" : "sur le vol stationnaire, tandis que le défilement et les changements de position ou la taille",
+	"always" : "toujours",
+	"How to" : "Comment puis-je utiliser cette extension",
+	"disable standard bars" : "Les barres de défilement standards d'Opera doivent être désactivé avant pouvoir voir celles-ci.<br>Pour ce faire, ouvre Opera → Réglages → Préférences ... → Avancé → Navigation et désactive \"Afficher les barres de défilement\"",
+	"footer" : "Merci d'avoir installé cette extension. Pour des commentaires et des questions, n'hésite pas à me contacter"
+};
---
+++ locales/it/strings.js
@@ -0,0 +1,18 @@
+var strings = {
+	"by" : "di",
+	"Options" : "Opzioni",
+	"look" : "Apparenza",
+	"color" : "Colore",
+	"size" : "Width (respectively height for horizontal bars) in normal state",
+	"hover_size" : "Width (respectively height for horizontal bars) when hovered with the mouse",
+	"border_radius" : "radius of the border's curve",
+	"no_bar_bg" : "hide background bars",
+	"behavior" : "Behavior",
+	"show_when" : "When do you want the bars to show up",
+	"only_over" : "only when hovered with the mouse",
+	"normal" : "upon hovering, while scrolling and at changes of position or size",
+	"always" : "sempre",
+	"How to" : "How do I use this extension",
+	"disable standard bars" : "Opera's standard scrollbars have to be disabled in order to be able to see these ones.<br>Per disattivare la barre di scorrimento predefinite andare in Opera → Preferenze → Avanzate → Navigazione e disattivare \"Barre di scorrimento\"",
+	"footer" : "Thanks for installing this extension. For feedback and questions don't hesitate to contact me"
+};
