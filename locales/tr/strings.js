ecd6e069a458594e360a8b7d66c05e7557784732
metude <metude@gmail.com>
2012-10-08 20:49:42 +0300

    turkish translation

---
+++ locales/tr/strings.js
@@ -0,0 +1,18 @@
+var strings = {
+	"by" : "by",
+	"Options" : "Ayarlar",
+	"look" : "Görünüm",
+	"color" : "Renk",
+	"size" : "Genişlik (respectively height for horizontal bars) normal durumdaki",
+	"hover_size" : "Genişlik (respectively height for horizontal bars) fare ile üzerine gelindiğinde",
+	"border_radius" : "kenarın kıvrılma durumu",
+	"no_bar_bg" : "arkaplan çubuklarını gizle",
+	"behavior" : "Davranış",
+	"show_when" : "Çubukların ne zaman görünmesini istiyorsunuz",
+	"only_over" : "fare ile üzerine gelince",
+	"normal" : "üzerine gelince, kaydırırken ve konum veya boyutunu değiştirirken",
+	"always" : "her zaman",
+	"How to" : "Bu eklentiyi nasıl kullanabilirim",
+	"disable standard bars" : "Bu eklentiyi kullanabilmek için Opera'nın standart kaydırma çubukları kapatılmalı.<br>Bunu yapmak için, Opera → Ayarlar → Tercihler... → Gelişmiş → Tarama ve \"kaydırma çubuklarını göster\" deki işareti kaldırın",
+	"footer" : "Bu eklentiyi kurduğunuz için teşekkürler. Geribildirim ve yorumlar için benimle iletişime geçmekten çekinmeyin"
+};
