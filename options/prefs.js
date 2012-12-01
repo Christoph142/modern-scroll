a1861d4e9d4285c0ac5938257bccb7c57f32eb91
Christoph <Christoph142@myopera.com>
2012-12-01 13:57:56 +0100

    new scrolling speed setting

--- options/prefs.js
+++ options/prefs.js
@@ -44,6 +44,11 @@ function getprefs(){
 	
 	if(document.getElementById("show_buttons").checked) document.getElementById("button_container").style.height = "auto";
 	if(!document.getElementById("show_superbar").checked) document.getElementById("superbar_container").style.height = "0px";
-	if(!document.getElementById("use_own_scroll_functions").checked) document.getElementById("functions_container").style.height = "0px";
+	if(!document.getElementById("use_own_scroll_functions").checked){
+		document.getElementById("functions_container").style.display="none";
+		if(!document.getElementById('animate_scroll').checked) document.getElementById('scroll_container').style.display="none";
+		else document.getElementById('scroll_v_label').innerHTML=strings['scroll_velocity auto'];
+	}
+	else if(document.getElementById('animate_scroll').checked) document.getElementById('scroll_v_label').innerHTML=strings['scroll_velocity both'];
 	document.getElementById("border_radius").max = Math.round(Math.max(document.getElementById("size").value, document.getElementById("hover_size").value)/2);
 }
