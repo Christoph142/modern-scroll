f0aa1639add3fe1cdae62866925be9b38f5fc3e8
ahoj1234 <d.drahomir@seznam.cz>
2013-02-16 12:47:42 +0100

    Update locales/cs/strings.js

--- locales/cs/strings.js
+++ locales/cs/strings.js
@@ -34,8 +34,8 @@ var strings = {
 	"always" : "Vždy",
 	"show_bg_bars_when" : "A lišty v pozadí posuvníků?",
 	"like bars" : "Jako posuvníky",
-	"fullscreen_only" : "Show the bars in fullscreen mode only",//"Používat moderní rolování jen v celoobrazovkovém režimu"
-	"fullscreen_only_short" : "Only in fullscreen mode",
+	"fullscreen_only" : "Zobrazovat posuvníky jen v celoobrazovkovém režimu",//"Používat moderní rolování jen v celoobrazovkovém režimu"
+	"fullscreen_only_short" : "Pouze v celoobrazovkovém režimu",
 	"move_bars_during_scroll" : "Animovat rolovací posuvníky během rolování kolečkem na myši / klávesnicí",
 	"bg_special_ends" : "Kliknutí na konec lišty v pozadí posuvníků sroluje úplně nahoru/dolů (respektive doleva/doprava) místo o jednu stránku",
 	"container1" : "Spustit moderní rolování ve vzdálenosti od",
