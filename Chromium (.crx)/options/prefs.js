064ef05f786f3f662137c2473c68ebc296d3b622
Christoph <christoph142@myopera.com>
2013-05-30 14:09:12 +0200

    fix show_buttons container expansion

--- Chromium (.crx)/options/prefs.js
+++ Chromium (.crx)/options/prefs.js
@@ -1,24 +1,21 @@
-window.addEventListener("DOMContentLoaded", getprefs, false);
+window.addEventListener("DOMContentLoaded", restoreprefs, false);
 window.addEventListener("DOMContentLoaded", localize, false);
-function _(to_translate){ console.log( chrome.i18n.getMessage(to_translate) ); }
+window.addEventListener("mouseup", save_buttonposition, false);
 
-// save preferences:
-window.addEventListener("change", function(e)
+window.addEventListener("change", function(e) // save preferences:
 {
 	if(e.target.id === "save_set" || e.target.id === "saved_sets") return; // handled via onclick funtions
 	
-	var saveobject = {};
-	if(e.target.type === "checkbox") saveobject[e.target.id] = e.target.checked?"1":"0";
-	else 							 saveobject[e.target.id] = e.target.value;
-	chrome.storage.sync.set(saveobject);
+	if(e.target.type === "checkbox") save_new_value(e.target.id, e.target.checked?"1":"0");
+	else 							 save_new_value(e.target.id, e.target.value);
 	
-	if(e.target.id === "border_color") chrome.storage.sync.set({ "border_color_rgba" : "rgba("+parseInt(e.target.value.substring(1,3),16)+","+parseInt(e.target.value.substring(3,5),16)+","+parseInt(e.target.value.substring(5,7),16)+",0.7)" });
+	if(e.target.id === "border_color") save_new_value("border_color_rgba", "rgba("+parseInt(e.target.value.substring(1,3),16)+","+parseInt(e.target.value.substring(3,5),16)+","+parseInt(e.target.value.substring(5,7),16)+",0.7)");
 	
 	if(e.target.id === "size" || e.target.id === "hover_size"){
 		document.getElementById("border_radius").max = Math.round(Math.max(document.getElementById("size").value, document.getElementById("hover_size").value)/2);
 		if(document.getElementById("border_radius").value > document.getElementById("border_radius").max){
 			document.getElementById("border_radius").value = document.getElementById("border_radius").max;
-			chrome.storage.sync.set({ "border_radius" : document.getElementById("border_radius").max });
+			save_new_value("border_radius", document.getElementById("border_radius").max);
 		}
 	}
 	
@@ -43,19 +40,24 @@ window.addEventListener("change", function(e)
 	
 },false);
 
-window.addEventListener("mouseup", save_buttonposition, false);
-function save_buttonposition(){
-	if(document.getElementsByClassName("dragged_button")[0])
-	{
-		chrome.storage.sync.set({ "buttonposition" : (100 * document.getElementsByClassName("dragged_button")[0].offsetLeft / window.innerWidth) });
-		chrome.extension.sendMessage({data:"update_optionspage"});
-	}
+function save_new_value(key, value)
+{
+	var saveobject = { key : value };
+	chrome.storage.sync.set(saveobject);					// save it in Chrome's synced storage
+	chrome.extension.getBackgroundPage().w[key] = value;	// update settings in background.js
+	
+	chrome.extension.sendMessage({data:"update_optionspage"});
 }
 
-function getprefs(){ chrome.storage.sync.get( null, function(storage){ restoreprefs(storage); } ); }
+function save_buttonposition(){
+	if(document.getElementsByClassName("dragged_button")[0])
+		save_new_value("buttonposition", (100 * document.getElementsByClassName("dragged_button")[0].offsetLeft / window.innerWidth));
+}
 
-function restoreprefs(storage)
+function restoreprefs()
 {
+	var storage = chrome.extension.getBackgroundPage().w;
+	
 	document.getElementById("save_set").innerHTML = chrome.i18n.getMessage("new_set_name");
 	document.getElementById("save_set").addEventListener("blur",function(){
 		if(this.innerHTML === "") this.innerHTML = chrome.i18n.getMessage("new_set_name");
@@ -87,13 +89,11 @@ function restoreprefs(storage)
 		else document.getElementsByTagName("select")[i].value = storage[selects[i].id];
 	}
 	
-	if(document.getElementById("show_buttons").value !== "1")	document.getElementById("button_container").style.height = "auto";
-	if(!document.getElementById("show_superbar").checked)		document.getElementById("superbar_container").style.height = "0px";
-	if(!document.getElementById("use_own_scroll_functions").checked)
-		document.getElementById("keyscroll_velocity_container").style.display="none";
-	if(!document.getElementById("use_own_scroll_functions_mouse").checked)
-		document.getElementById("mousescroll_container").style.display="none";
-	if(!document.getElementById("animate_scroll").checked)		document.getElementById("scroll_container").style.display = "none";
+	if(document.getElementById("show_buttons").value !== "1")				document.getElementById("button_container").style.height				= "auto";
+	if(document.getElementById("show_superbar").checked)					document.getElementById("superbar_container").style.height				= "auto";
+	if(!document.getElementById("use_own_scroll_functions").checked)		document.getElementById("keyscroll_velocity_container").style.display	= "none";
+	if(!document.getElementById("use_own_scroll_functions_mouse").checked)	document.getElementById("mousescroll_container").style.display			= "none";
+	if(!document.getElementById("animate_scroll").checked)					document.getElementById("scroll_container").style.display				= "none";
 	
 	document.getElementById("border_radius").max = Math.round(Math.max(document.getElementById("size").value, document.getElementById("hover_size").value)/2);
 	
@@ -103,10 +103,6 @@ function restoreprefs(storage)
 		var raw_value = (storage[which_value] ? storage[which_value] : document.getElementById(which_value).value);
 		document.getElementsByClassName("slider_values")[i].innerHTML = (document.getElementById(which_value).dataset.defaultvalue ? Math.floor(100*raw_value/document.getElementById(which_value).dataset.defaultvalue) : raw_value);
 		// ########################################################################################################################### why floor?! ####
-		
-		/*document.getElementById(which_value).addEventListener("change", function(){ // update display on change of sliders:
-			document.getElementById("storage."+which_value).innerHTML = storage[which_value];
-		},false);*/
 	}
 	
 	for(var i=0; i<document.getElementsByClassName("i").length; i++){ // information boxes:
@@ -217,7 +213,7 @@ function localize()
 	{
 		if(strings[i].tagName === "IMG")		strings[i].title = chrome.i18n.getMessage(strings[i].title); // tooltips
 		else if(strings[i].tagName === "LABEL")	strings[i].innerHTML += chrome.i18n.getMessage( (!strings[i].id ? strings[i].htmlFor : strings[i].id) );
-		else									strings[i].innerHTML += chrome.i18n.getMessage(strings[i].id);
+		else									strings[i].innerHTML += chrome.i18n.getMessage( (!strings[i].id ? strings[i].dataset.i18n : strings[i].id));
 	}
 }
 
